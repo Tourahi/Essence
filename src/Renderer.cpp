262e30994899aad60ab4e6af44a1954cfc6d62e4
Tourahi <amine-fb-fb@hotmail.fr>
2024-09-16 22:45:04 +0100

    Static helpers.

--- src/Renderer.cpp
+++ src/Renderer.cpp
@@ -1,4 +1,5 @@
 #include <SDL2/SDL_video.h>
+#include <cstdlib>
 #include <stdio.h>
 #include <stdbool.h>
 #include <assert.h>
@@ -48,3 +49,30 @@ void RInit(SDL_Window *win)
   SDL_Surface *surf = SDL_GetWindowSurface(window);
   RSetClipRect( (RRect) {0, 0, surf->w, surf->h} );
 }
+
+static void* checkAlloc (void *ptr)
+{
+  if (!ptr) {
+    fprintf(stderr, "Fatal error: Memory allocation failed\n");
+    exit(EXIT_FAILURE);
+  }
+  return ptr;
+}
+
+static const char* utf8ToCodepoint (const char *p; unsigned *dst)
+{
+  unsigned res, n;
+  switch (*p & 0xf0) {
+    case 0xf0 :  res = *p & 0x07;  n = 3;  break;
+    case 0xe0 :  res = *p & 0x0f;  n = 2;  break;
+    case 0xd0 :
+    case 0xc0 :  res = *p & 0x1f;  n = 1;  break;
+    default   :  res = *p;         n = 0;  break;
+  }
+  while (n--) {
+    res = (res << 6) | (*(++p) & 0x3f);
+  }
+  *dst = res;
+  return p + 1;
+}
+
